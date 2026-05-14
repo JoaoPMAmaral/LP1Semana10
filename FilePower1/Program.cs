494edbf92e84e782b705192a4b9df4e57f3c2eab
Joao A <joao.pedro.matias.amaral@gmail.com>
2026-05-14 15:05:26 +0100

    created equalplayer filepower intcollections, updated solution

---
+++ FilePower1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FilePower1
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string nomeFicheiro = args[0];
+            StreamWriter sw = new StreamWriter(nomeFicheiro);
+            Queue<string> palavras = new Queue<string>();
+
+            bool loop = true;
+
+            while(loop == true)
+            {
+                
+                Console.WriteLine("Escreve uma palavra");
+                string palavra = Console.ReadLine();
+
+                if(palavra == "")
+                {
+                    loop = false;
+                }
+                
+                palavras.Enqueue(palavra);
+            }
+
+            foreach(string elemento in palavras)
+            {
+                sw.WriteLine(elemento);
+            }
+
+            sw.Close();
+        }
+    }
+}
---
+++ IntCollections/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IntCollections
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            List<int> list = new List<int>(){1,10,-30,10,-5};
+            Stack<int> stack = new Stack<int>(new[]{1,10,-30,10,-5});
+            Queue<int> queue = new Queue<int>(new[]{1,10,-30,10,-5});
+            HashSet<int> hash = new HashSet<int>(){1,10,-30,10,-5};
+
+            Console.Write("List: ");
+            foreach(int number in list)
+            {
+                Console.Write($"{number} ");
+            }
+
+            Console.WriteLine();
+            Console.Write("Stack: ");
+            foreach(int number in stack)
+            {
+                Console.Write($"{number} ");
+            }
+
+            Console.WriteLine();
+            Console.Write("Queue: ");
+            foreach(int number in queue)
+            {
+                Console.Write($"{number} ");
+            }
+
+            Console.WriteLine();
+            Console.Write("HashSet: ");
+            foreach(int number in hash)
+            {
+                Console.Write($"{number} ");
+            }
+        }
+    }
+}
